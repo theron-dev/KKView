ad868942580337b186c75b328c2fbb30adb420c6
hailong11 <hailong11@staff.weibo.com>
2018-05-11 18:24:59 +0800

    update

--- KKView/KKView/KKControlViewElement.m
+++ KKView/KKView/KKControlViewElement.m
@@ -25,7 +25,7 @@
 
 -(void) setView:(UIView *)view {
     UIControl * v = (UIControl *) self.view;
-    [v removeTarget:self action:@selector(doTapAction) forControlEvents:UIControlEventTouchUpInside];
+    [v removeTarget:self action:@selector(doTapAction:event:) forControlEvents:UIControlEventTouchUpInside];
     [self.view removeObserver:self forKeyPath:@"enabled"];
     [self.view removeObserver:self forKeyPath:@"selected"];
     [self.view removeObserver:self forKeyPath:@"highlighted"];
@@ -34,14 +34,24 @@
     [self.view addObserver:self forKeyPath:@"selected" options:NSKeyValueObservingOptionNew context:nil];
     [self.view addObserver:self forKeyPath:@"highlighted" options:NSKeyValueObservingOptionNew context:nil];
     v = (UIControl *) self.view;
-    [v addTarget:self action:@selector(doTapAction) forControlEvents:UIControlEventTouchUpInside];
+    [v addTarget:self action:@selector(doTapAction:event:) forControlEvents:UIControlEventTouchUpInside];
 }
 
--(void) doTapAction {
+-(void) doTapAction:(id) sender event:(UIEvent *) event {
     
     KKElementEvent * e = [[KKElementEvent alloc] initWithElement:self];
 
-    e.data = [self data];
+    NSMutableDictionary * data = [self data];
+    
+    CGPoint p = [[[event allTouches] anyObject] locationInView:self.view];
+    CGSize size = self.view.bounds.size;
+    
+    data[@"x"] = @(p.x);
+    data[@"y"] = @(p.y);
+    data[@"width"] = @(size.width);
+    data[@"height"] = @(size.height);
+    
+    e.data = data;
     
     [self emit:@"tap" event:e];
     
@@ -53,7 +63,7 @@
     [self.view removeObserver:self forKeyPath:@"selected"];
     [self.view removeObserver:self forKeyPath:@"highlighted"];
     UIControl * v = (UIControl *) self.view;
-    [v removeTarget:self action:@selector(doTapAction) forControlEvents:UIControlEventTouchUpInside];
+    [v removeTarget:self action:@selector(doTapAction:event:) forControlEvents:UIControlEventTouchUpInside];
 }
 
 -(void) observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSKeyValueChangeKey,id> *)change context:(void *)context {
